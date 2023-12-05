ae2cf93f56b1ae57a6ec9e5859764dda82d309dd
Ruslan <ruslan.kuptsov@local-internet.ru>
2023-12-05 20:40:42 +0300

    update

--- index.php
+++ index.php
@@ -7,5 +7,18 @@ $test = '1241412';
 $test = '1241412';
 $test = '1241412';
 $test = '1241412';
+
+
+$new = "1232421";
+
+$hello = "func";
+
+
+function newTest(){
+    return 1
+}
+
+print(11)
+
 print $test;
 
