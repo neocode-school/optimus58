54c990ce18eb438fe088c861464e4b34a194a418
Ruslan <ruslan.kuptsov@local-internet.ru>
2024-01-10 21:10:35 +0300

    add new code

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = strip_tags(trim($_POST["message"]));
 
-$my_name = 'Max22';
-function test2(){
-    return 111;
+    if (empty($name) || !filter_var($email, FILTER_VALIDATE_EMAIL) || empty($message)) {
+        echo "Пожалуйста, заполните форму корректно.";
+        exit;
+    }
+    $recipient = "your@email.com";
+    $subject = "Новое сообщение от $name";
+
+    $email_content = "Имя: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Сообщение:\n$message\n";
+
+    $email_headers = "From: $name <$email>";
+
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        echo "Сообщение успешно отправлено.";
+    } else {
+        echo "Не удалось отправить сообщение.";
+    }
+} else {
+    // Не POST запрос, перенаправляем на главную страницу
+    header("Location: index.html");
+    exit;
 }
-
-foreach (range(1,1000) as $value){
-    print $value;
-}
-
-
-test2();
+?>
